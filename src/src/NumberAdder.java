94782c43c7d1d1227507d4cd70fe7f43958ca94f
lavdrim.selimi <lavdrim.selimi.learning@gmail.com>
2023-04-11 17:50:45 +0200

    Added NumberAdder.

---
+++ src/src/NumberAdder.java
@@ -0,0 +1,11 @@
+public class NumberAdder {
+    public static void main(String[] args) {
+
+        int num1 = 2;
+        int num2 = 3;
+
+        int total = num1 + num2;
+
+        System.out.println("Total: " + total);
+    }
+}
